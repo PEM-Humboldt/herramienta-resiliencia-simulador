7fd8bc34aa0590067d66f31f96469fec21a088b7
Jorge Amador <jaamadorm@unal.edu.co>
2022-01-14 10:41:38 -0500

    Save outputs of the model as .csv files within the ignored directory ./outputs/

--- condiciones_iniciales/initial_cond_cover.py
+++ condiciones_iniciales/initial_cond_cover.py
@@ -1,18 +1,16 @@
 
 import pandas as pd
 from sqlalchemy import create_engine
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
+# MONGODB configuration
 
 def initial_cover(num_digits):
-    POSTGRES_ADDRESS = os.getenv('POSTGRES_ADDRESS')
-    POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-    POSTGRES_USERNAME = os.getenv('POSTGRES_USERNAME')
-    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-    POSTGRES_DBNAME = os.getenv('POSTGRES_DBNAME')
+    
+# Postgres username, password, and database name
+    POSTGRES_ADDRESS = '192.168.11.106' ## INSERT YOUR DB ADDRESS IF IT'S NOT ON PANOPLY
+    POSTGRES_PORT = '5454'
+    POSTGRES_USERNAME = 'eco'
+    POSTGRES_PASSWORD = 'eco'
+    POSTGRES_DBNAME = 'eco'
     # Do not change this long string that contains the necessary MongoDB login information
     postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(username=POSTGRES_USERNAME,
                                                                                             password=POSTGRES_PASSWORD,
@@ -22,36 +20,54 @@ def initial_cover(num_digits):
     # Create the connection
     cnx = create_engine(postgres_str)
     
-    ### Consulta SQL para obtener las capas
+    ### query SQL to get data
     
     layers = pd.read_sql_query('SELECT * FROM public.cobert_tillava', cnx)
     
-    # Crea una copia de la capa para posibles modificaciones
+    # Create a copy of the layer for possible modifications
     layers_new = layers.rename(columns={'codigo_clc':'codigo_clc_new'})
     
-    # define el nivel a usar en la agrupación de capas ¿debería ser menor o igual a min_level?
-    # num_digits = 2
+    # Determines the number of existing categories in the shape layer
+    column_values = layers_new[["cobertura"]].values.ravel()
     
-    cod = layers_new['codigo_clc_new']
-    cod_new = [None for cod in range(len(cod))]
-    
-    for i in range(len(cod)):
-        num_str = str(cod[i])
-        cod_new[i] = len(num_str)
-        layers_new['codigo_clc_new'][i] = num_str[0:num_digits]
-    
-    # min_level = min(cod_new)
-    
-    column_values = layers_new[["codigo_clc_new"]].values.ravel()
-    unique_values =  pd.unique(column_values) # numero de coberturas diferentes para el nivel establecido
+    # number of different covers
+    unique_values =  pd.unique(column_values)
     n = len(unique_values)
-    # sum_cover = [None for unique_values in range(n)]
+    
+    
     sum_cover = [[None for unique_values in range(2)] for unique_values in range(n)]
      
-    # crea una lista con los digitos del nivel y la suma de las shape_area ¿Cuales son las unidades de área?
+    # Creates a list with the category name and sum of area
     for i in range(len(unique_values)):
-        idx = layers_new.index[layers_new['codigo_clc_new'] == unique_values[i]].tolist()
+        idx = layers_new.index[layers_new['cobertura'] == unique_values[i]].tolist()
         sum_cover[i][0] = unique_values[i]
         sum_cover[i][1] = sum(layers_new.iloc[idx]["area_ha"])
         
     return sum_cover
+
+    # # define el nivel a usar en la agrupación de capas ¿debería ser menor o igual a min_level?
+    # # num_digits = 2
+    
+    # cod = layers_new['codigo_clc_new']
+    # cod_new = [None for cod in range(len(cod))]
+    
+    # for i in range(len(cod)):
+    #     num_str = str(cod[i])
+    #     cod_new[i] = len(num_str)
+    #     layers_new['codigo_clc_new'][i] = num_str[0:num_digits]
+    
+    # # min_level = min(cod_new)
+    
+    # column_values = layers_new[["codigo_clc_new"]].values.ravel()
+    # unique_values =  pd.unique(column_values) # numero de coberturas diferentes para el nivel establecido
+    # n = len(unique_values)
+    # # sum_cover = [None for unique_values in range(n)]
+    # sum_cover = [[None for unique_values in range(2)] for unique_values in range(n)]
+     
+    # # crea una lista con los digitos del nivel y la suma de las shape_area ¿Cuales son las unidades de área?
+    # for i in range(len(unique_values)):
+    #     idx = layers_new.index[layers_new['codigo_clc_new'] == unique_values[i]].tolist()
+    #     sum_cover[i][0] = unique_values[i]
+    #     sum_cover[i][1] = sum(layers_new.iloc[idx]["area_ha"])
+        
+    # return sum_cover
--- run_principal.py
+++ run_principal.py
@@ -6,20 +6,21 @@ import initial_cond_cover as icc
 import fun_ode as ode
 from scipy.integrate import odeint
 import numpy as np
-import random # puede eliminarse cuando se tengan los valores "reales" de las tasas de cambio de coberturas
 import matplotlib.pyplot as plt
+import pandas as pd
+import random # puede eliminarse cuando se tengan los valores "reales" de las tasas de cambio de coberturas
 
-
-num_digits = 1 # define el nivel a usar en la agrupación de capas ¿debería ser menor o igual a min_level?
+num_digits = 2 # define el nivel a usar en la agrupación de capas ¿debería ser menor o igual a min_level?
 data0_cover = sorted(icc.initial_cover(num_digits))
 
-# condiciones iniciales de coberturas
-x0_cover = [row[1] for row in data0_cover]  
+# initial conditions for soil covers
+x0_cover = [row[1] for row in data0_cover]
+name_cover = [row[0] for row in data0_cover]
 nc = len(x0_cover)
 
 rates_cover = [[None for x0_cober in range(nc)] for x0_cover in range(nc)]
-# se genera una matriz con los valores de las tasas de transformación de coberturas
-# para el caso i=j, la tasa es cero - uan covertura no se transforma en si misma, o si?
+# generates a matrix with the values of the coverage transformation rates
+# for the case i = j, the rate is zero - a cover does not transform itself
 for i in range(nc):
     for j in range(nc):
         if i == j:
@@ -27,8 +28,7 @@ for i in range(nc):
         else:
             rates_cover[i][j] = random.uniform(0.01,0.02)
 
-
-# configuración del integrador
+# INTEGRATOR CONFIGURATION
 
 tmin = 2020
 tmax = 2030
@@ -40,16 +40,22 @@ rates_cover = np.array(rates_cover)
 rates_cover_t = rates_cover.transpose()
 nx0 = len(x0_cover)
 
-# integración por RK45
+# Integration by RK45
 Ys = odeint(ode.differential_equations, x0_cover, time, args=(rates_cover, rates_cover_t))
-Yt = np.sum(Ys,axis=1) # axis=1 --> suma silas, axis=0 --> suma columnas
+Yt = np.sum(Ys,axis=1) # axis=1 --> add rows, axis=0 --> add columns
 
+# Export time series as .csv
+cover_time_series = pd.DataFrame(Ys, columns=name_cover)
+cover_time_series.to_csv('./outputs/cover_time_series.csv', float_format='%.2f')
+
+
+# OPTIONAL - PLOT TIME SERIES
 for i in range(nx0):
     j = i + 1
-    plt.plot(time, Ys[:, i], label='%s cobertura' % j)
+    plt.plot(time, Ys[:, i], label=name_cover[i])
     plt.scatter(time, Ys[:, i])
 
-plt.plot(time, Yt, label='área total')
+plt.plot(time, Yt, label='Área total')
 plt.legend(loc='best')
 plt.xlabel('tiempo')
 plt.grid()
