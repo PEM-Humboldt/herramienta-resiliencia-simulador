04406f6e79dfe14fc9c2b476e64dba8c24ba6dcf
Jorge Amador <jaamadorm@unal.edu.co>
2022-04-05 13:42:14 -0500

    load transformation rates from Excel file

--- run_principal.py
+++ run_principal.py
@@ -1,5 +1,6 @@
 
 import sys
+from trace import CoverageResults
 sys.path.insert(0, './condiciones_iniciales')
 
 import initial_cond_cover as icc 
@@ -17,15 +18,20 @@ x0_cover = [row[1] for row in data0_cover]
 name_cover = [row[0] for row in data0_cover]
 nc = len(x0_cover)
 
-cover_rates = [[None for x0_cober in range(nc)] for x0_cover in range(nc)]
-# generates a matrix with the values of the coverage transformation rates
-# for the case i = j, the rate is one - a cover does not transform itself
-for i in range(nc):
-    for j in range(nc):
-        if i == j:
-            cover_rates[i][j] = 0
-        else:
-            cover_rates[i][j] = random.uniform(0.01,0.02)
+# conda install openpyxl
+df = pd.read_excel (r'./condiciones_iniciales/parameters.xlsx', sheet_name='transformation_rates')
+arr = df.to_numpy()
+cover_rates = arr[0:nc,1:nc+1]
+
+# cover_rates = [[None for x0_cober in range(nc)] for x0_cover in range(nc)]
+# # generates a matrix with the values of the coverage transformation rates
+# # for the case i = j, the rate is one - a cover does not transform itself
+# for i in range(nc):
+#     for j in range(nc):
+#         if i == j:
+#             cover_rates[i][j] = 0
+#         else:
+#             cover_rates[i][j] = random.uniform(0.005,0.01)
 
 # INTEGRATOR CONFIGURATION
 
@@ -49,13 +55,13 @@ cover_time_series.to_csv('./outputs/cover_time_series.csv', float_format='%.2f')
 
 
 # OPTIONAL - PLOT TIME SERIES
-# for i in range(nx0):
-#     j = i + 1
-#     plt.plot(time, Ys[:, i], label=name_cover[i])
-#     plt.scatter(time, Ys[:, i])
+for i in range(nx0):
+    j = i + 1
+    plt.plot(time, Ys[:, i], label=name_cover[i])
+    plt.scatter(time, Ys[:, i])
 
-# plt.plot(time, Yt, label='Área total')
-# plt.legend(loc='best')
-# plt.xlabel('tiempo')
-# plt.grid()
-# plt.show()
+plt.plot(time, Yt, label='Área total')
+plt.legend(loc='best')
+plt.xlabel('tiempo')
+plt.grid()
+plt.show()
