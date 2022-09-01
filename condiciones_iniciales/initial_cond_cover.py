64a22e4cb9c69ccbaed107eb6e7ff6e20ee0add5
Manuel Gálvez <mgalvez@humboldt.org.co>
2022-09-01 16:17:09 -0500

    Database connection and query format through centralized functions

--- condiciones_iniciales/initial_cond_cover.py
+++ condiciones_iniciales/initial_cond_cover.py
@@ -1,44 +1,19 @@
-
 import pandas as pd
 from sqlalchemy import create_engine
-import os
 from dotenv import load_dotenv
+from db_functions import db_connect, set_query
 
 load_dotenv()
 
 def initial_cover(workspace):
-    DB_SYSTEM = os.getenv('DB_SYSTEM')
-    DB_ADDRESS = os.getenv('DB_ADDRESS')
-    DB_PORT = os.getenv('DB_PORT')
-    DB_USERNAME = os.getenv('DB_USERNAME')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_NAME = os.getenv('DB_NAME')
 
-    if DB_SYSTEM=='oracle':
-        SQL_DRIVER = 'cx_oracle'
-        connection_str = ('{dbsystem}+{sqldriver}://{username}:{password}@{ipaddress}:{port}/?service_name={dbname}'.format(dbsystem=DB_SYSTEM,
-                                                                                                sqldriver=SQL_DRIVER,
-                                                                                                username=DB_USERNAME,
-                                                                                                password=DB_PASSWORD,
-                                                                                                ipaddress=DB_ADDRESS,
-                                                                                                port=DB_PORT,
-                                                                                                dbname=DB_NAME))
-        data_query = f'SELECT codigo_clc, cobertura, area_ha FROM {workspace}_coberturas'
-    else:
-        # Do not change this long string that contains the necessary MongoDB login information
-        connection_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(dbsystem=DB_SYSTEM,
-                                                                                                username=DB_USERNAME,
-                                                                                                password=DB_PASSWORD,
-                                                                                                ipaddress=DB_ADDRESS,
-                                                                                                port=DB_PORT,
-                                                                                                dbname=DB_NAME))
-        data_query = f'SELECT codigo_clc, cobertura, area_ha FROM public.{workspace}_coberturas'
-
-    # Create the connection
-    cnx = create_engine(connection_str)
+    module = 'coberturas'
+    fields = ['codigo_clc', 'cobertura', 'area_ha']
+    
+    cnx = db_connect()
 
     ### query SQL to get data
-
+    data_query = set_query(workspace, module, fields)
     layers = pd.read_sql_query(data_query, cnx)
 
     # Create a copy of the layer for possible modifications
