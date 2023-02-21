a3c7b4832c073fdb5d82f35cefb24dd71d90b34b
Jorge Amador <jaamadorm@unal.edu.co>
2023-02-21 10:41:42 -0500

    udated the weights to the resilience indicator

--- resilience_indicators.py
+++ resilience_indicators.py
@@ -11,19 +11,19 @@ def slope_time_series(model_time_series, name_year,
                                                      name_all_mper_fun,
                                                      name_IDivAPro,
                                                      name_DivSisAlimLocal,
-                                                     name_PperxFun,
+                                                     name_PperxFun_nectar,
+                                                     name_PperxFun_frugi,
+                                                     name_PperxFun_semilla,
                                                      name_ConectBOn,
                                                      name_water,
                                                      name_SF,
                                                      name_Con_Acces,
                                                      name_p_fort_empren,
-                                                     name_sum_pyf,
                                                      name_p_fort_inclu,
                                                      name_SAConfli,
+                                                     name_sum_pyf,
                                                      name_IntCom,
-                                                     name_mv_modificada,
-                                                     name_mv_AirQ,
-                                                     name_mv_NoiseAte_d,
+                                                     name_Health,
                                                      AT,
                                                      name_PT,
                                                      posi2):
@@ -31,7 +31,7 @@ def slope_time_series(model_time_series, name_year,
     names_normalized_cover = np.concatenate((name_NA, names_HeterAg))
     normalized_covers = model_time_series.loc[:, names_normalized_cover] / AT
     
-    names_normalized_maxa = np.concatenate((name_Land_Div, name_water, name_mv_NoiseAte_d))
+    names_normalized_maxa = np.concatenate((name_Land_Div, name_water))
     data_maxa = model_time_series.loc[:, names_normalized_maxa]
     maxa = data_maxa.max()
     normalized_maxa = data_maxa / maxa 
@@ -48,8 +48,8 @@ def slope_time_series(model_time_series, name_year,
     normalized_IntCom = model_time_series.loc[:, name_IntCom] / 5
     
     names_normalized_variables = np.concatenate((name_all_mper_Esp, name_all_mper_fun, name_IDivAPro, name_DivSisAlimLocal,
-                                           name_PperxFun, name_ConectBOn, name_SF, name_Con_Acces, name_sum_pyf,
-                                           name_mv_modificada, name_mv_AirQ))
+                                           name_PperxFun_nectar, name_PperxFun_frugi, name_PperxFun_semilla, name_ConectBOn,
+                                           name_SF, name_Con_Acces, name_sum_pyf, name_Health))
     
     normalized_variables = model_time_series.loc[:, names_normalized_variables]
     
@@ -61,10 +61,10 @@ def slope_time_series(model_time_series, name_year,
     
     all_names_variables = np.concatenate((name_Land_Div,names_DA, name_NA, names_HeterAg, name_all_mper_Esp,
                                                      name_all_mper_fun, name_IDivAPro, name_DivSisAlimLocal,
-                                                     name_PperxFun, name_ConectBOn, name_water,
-                                                     name_SF, name_Con_Acces, name_p_fort_empren,
-                                                     name_sum_pyf, name_p_fort_inclu, name_SAConfli,
-                                                     name_IntCom, name_mv_modificada, name_mv_AirQ,name_mv_NoiseAte_d
+                                                     name_PperxFun_nectar, name_PperxFun_frugi, name_PperxFun_semilla,
+                                                     name_ConectBOn, name_water, name_SF, name_Con_Acces, name_p_fort_empren,
+                                                     name_p_fort_inclu, name_SAConfli, name_sum_pyf,
+                                                     name_IntCom, name_Health
                                           ))
     
     indicators = [[None for x in range(len(all_names_variables))] for y in range(len(time))]
@@ -72,18 +72,15 @@ def slope_time_series(model_time_series, name_year,
     name_variables = [None for x in range(len(all_names_variables))]
     weigths = [None for x in range(len(all_names_variables))]
     
-    num_principles = 5
-    weight_principle = 1 / num_principles
     # principle 1
-    weight_proper_princ1 = weight_principle / 4
     # Diversity and redundance Indicators 
     # 1. Ladscape diversity
     name_prop_land_div = np.concatenate((name_Land_Div, names_DA, name_NA, names_HeterAg))
     data_prop_land_div = all_normalized_variables.loc[:, name_prop_land_div]
     num_var_land_div = len(name_prop_land_div)
-    weight_var_proper1_princ1 = weight_proper_princ1 / num_var_land_div
+    weight_var_proper1_princ1 = np.array([12, 13, 16 , 15]) / 180
     j = 0
-    for i in range(len(name_prop_land_div)):
+    for i in range(num_var_land_div):
         y = np.array(data_prop_land_div.loc[:, name_prop_land_div[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -93,7 +90,7 @@ def slope_time_series(model_time_series, name_year,
         xx_mean2 = sum((x - x_mean) ** 2)
         b1 = xx_mean_yy_mean / xx_mean2
         indicators[0][j] = b1
-        weigths[j] = weight_var_proper1_princ1
+        weigths[j] = weight_var_proper1_princ1[i]
         name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_land_div[i]
         name_variables[j] = name_prop_land_div[i]
         j = j + 1
@@ -102,8 +99,8 @@ def slope_time_series(model_time_series, name_year,
     name_prop_div_redun = np.concatenate((name_all_mper_Esp, name_all_mper_fun))
     data_prop_div_redun = all_normalized_variables.loc[:, name_prop_div_redun]
     num_var_div_redun = len(name_prop_div_redun)
-    weight_var_proper2_princ1 = weight_proper_princ1 / num_var_div_redun
-    for i in range(len(name_prop_div_redun)):
+    weight_var_proper2_princ1 = np.array([4, 5]) / 180
+    for i in range(num_var_div_redun):
         y = np.array(data_prop_div_redun.loc[:, name_prop_div_redun[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -113,7 +110,7 @@ def slope_time_series(model_time_series, name_year,
         xx_mean2 = sum((x - x_mean) ** 2)
         b1 = xx_mean_yy_mean / xx_mean2
         indicators[0][j] = b1
-        weigths[j] = weight_var_proper2_princ1
+        weigths[j] = weight_var_proper2_princ1[i]
         name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_div_redun[i]
         name_variables[j] = name_prop_div_redun[i]
         j = j + 1
@@ -122,8 +119,8 @@ def slope_time_series(model_time_series, name_year,
     name_prop_div_AcProd = name_IDivAPro
     data_prop_div_AcProd = all_normalized_variables.loc[:, name_prop_div_AcProd]
     num_var_div_AcProd = len(name_prop_div_AcProd)
-    weight_var_proper3_princ1 = weight_proper_princ1 / num_var_div_AcProd
-    for i in range(len(name_prop_div_AcProd)):
+    weight_var_proper3_princ1 = np.array([18]) / 180
+    for i in range(num_var_div_AcProd):
         y = np.array(data_prop_div_AcProd.loc[:, name_prop_div_AcProd[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -133,7 +130,7 @@ def slope_time_series(model_time_series, name_year,
         xx_mean2 = sum((x - x_mean) ** 2)
         b1 = xx_mean_yy_mean / xx_mean2
         indicators[0][j] = b1
-        weigths[j] = weight_var_proper3_princ1
+        weigths[j] = weight_var_proper3_princ1[i]
         name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_div_AcProd[i]
         name_variables[j] = name_prop_div_AcProd[i]
         j = j + 1
@@ -142,8 +139,8 @@ def slope_time_series(model_time_series, name_year,
     name_prop_div_ModVida = name_DivSisAlimLocal
     data_prop_div_ModVida = all_normalized_variables.loc[:, name_prop_div_ModVida]
     num_var_div_ModVida = len(name_prop_div_ModVida)
-    weight_var_proper4_princ1 = weight_proper_princ1 / num_var_div_ModVida
-    for i in range(len(name_prop_div_ModVida)):
+    weight_var_proper4_princ1 =  np.array([16]) / 180
+    for i in range(num_var_div_ModVida):
         y = np.array(data_prop_div_ModVida.loc[:, name_prop_div_ModVida[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -153,7 +150,7 @@ def slope_time_series(model_time_series, name_year,
         xx_mean2 = sum((x - x_mean) ** 2)
         b1 = xx_mean_yy_mean / xx_mean2
         indicators[0][j] = b1
-        weigths[j] = weight_var_proper4_princ1
+        weigths[j] = weight_var_proper4_princ1[i]
         name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_div_ModVida[i]
         name_variables[j] = name_prop_div_ModVida[i]
         j = j + 1
@@ -161,12 +158,11 @@ def slope_time_series(model_time_series, name_year,
     
     # Conectivity
     # 1. all variables
-    name_prop_conectivity = np.concatenate((name_PperxFun, name_ConectBOn, name_water, name_SF, name_Con_Acces))
+    name_prop_conectivity = np.concatenate((name_PperxFun_nectar, name_PperxFun_frugi, name_PperxFun_semilla, name_ConectBOn, name_water, name_SF, name_Con_Acces))
     data_prop_conectivity = all_normalized_variables.loc[:, name_prop_conectivity]
     num_var_div_conectivity = len(name_prop_conectivity)
-    weight_proper_princ2 = weight_principle / (num_var_div_conectivity - len(name_PperxFun) + len(posi2))
-    weight_var_proper_princ2 = weight_proper_princ2
-    for i in range(len(name_prop_conectivity)):
+    weight_var_proper_princ2 = np.array([5, 5, 5, 6, 16, 4, 4]) / 180
+    for i in range(num_var_div_conectivity):
         y = np.array(data_prop_conectivity.loc[:, name_prop_conectivity[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -177,25 +173,24 @@ def slope_time_series(model_time_series, name_year,
         b1 = xx_mean_yy_mean / xx_mean2
         if b1 == 0:
             indicators[0][j] = y[0]
-            weigths[j] = weight_var_proper_princ2
+            weigths[j] = weight_var_proper_princ2[i]
         else:
             indicators[0][j] = b1
-            weigths[j] = weight_var_proper_princ2
+            weigths[j] = weight_var_proper_princ2[i]
         name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_conectivity[i]
         name_variables[j] = name_prop_conectivity[i]
         j = j + 1
     len_2 = len(name_prop_conectivity)
         
     # principle 3
-    weight_proper_princ3 = weight_principle / 2
     # Learning and experimentation
     # 1. Innovation and Experimentation
     name_prop_InnovExper = name_p_fort_empren
     data_prop_InnovExper = all_normalized_variables.loc[:, name_prop_InnovExper]
     num_var_InnovExper = len(name_prop_InnovExper)
-    weight_var_proper1_princ3 = weight_proper_princ3 / num_var_InnovExper
+    weight_var_proper1_princ3 = np.array([4]) / 180
     
-    for i in range(len(name_prop_InnovExper)):
+    for i in range(num_var_InnovExper):
         y = np.array(data_prop_InnovExper.loc[:, name_prop_InnovExper[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -205,17 +200,17 @@ def slope_time_series(model_time_series, name_year,
         xx_mean2 = sum((x - x_mean) ** 2)
         b1 = xx_mean_yy_mean / xx_mean2
         indicators[0][j] = b1
-        weigths[j] = weight_var_proper1_princ3
+        weigths[j] = weight_var_proper1_princ3[i]
         name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_InnovExper[i]
         name_variables[j] =  name_prop_InnovExper[i]
         j = j + 1
     
      # 2. Transmission of knowledge - training
-    name_prop_transmission = np.concatenate((name_sum_pyf, name_p_fort_inclu))
+    name_prop_transmission = name_p_fort_inclu
     data_prop_transmission = all_normalized_variables.loc[:, name_prop_transmission]
     num_var_transmission = len(name_prop_transmission)
-    weight_var_proper2_princ3 = weight_proper_princ3 / num_var_transmission
-    for i in range(len(name_prop_transmission)):
+    weight_var_proper2_princ3 = np.array([4]) / 180
+    for i in range(num_var_transmission):
         y = np.array(data_prop_transmission.loc[:, name_prop_transmission[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -226,24 +221,24 @@ def slope_time_series(model_time_series, name_year,
         b1 = xx_mean_yy_mean / xx_mean2
         if b1 == 0:
             indicators[0][j] = y[0]
-            weigths[j] = weight_var_proper2_princ3
+            weigths[j] = weight_var_proper2_princ3[i]
         else:
             indicators[0][j] = b1
-            weigths[j] = weight_var_proper2_princ3
+            weigths[j] = weight_var_proper2_princ3[i]
         name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_transmission[i]
         name_variables[j] = name_prop_transmission[i]
         j = j + 1
     len_3 = len(np.concatenate((name_prop_InnovExper, name_prop_transmission)))
     
     # principle 4
-    weight_proper_princ4 = weight_principle / 1
+
     # Participation for equity
     # collaboration between actors
     name_prop_Colaboration = name_SAConfli
     data_prop_Colaboration = all_normalized_variables.loc[:, name_prop_Colaboration]
     num_var_Colaboration = len(name_prop_Colaboration)
-    weight_var_proper1_princ4 = weight_proper_princ4 / num_var_Colaboration
-    for i in range(len(name_prop_Colaboration)):
+    weight_var_proper1_princ4 = np.array([6]) / 180
+    for i in range(num_var_Colaboration):
         y = np.array(data_prop_Colaboration.loc[:, name_prop_Colaboration[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -253,22 +248,22 @@ def slope_time_series(model_time_series, name_year,
         xx_mean2 = sum((x - x_mean) ** 2)
         b1 = xx_mean_yy_mean / xx_mean2
         indicators[0][j] = b1
-        weigths[j] = weight_var_proper1_princ4
+        weigths[j] = weight_var_proper1_princ4[i]
         name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_Colaboration[i]
         name_variables[j] = name_prop_Colaboration[i]
         j = j + 1
     len_4 = len(name_prop_Colaboration)
     
     # principle 5
-    weight_proper_princ5 = weight_principle / 1
+
     # Polycentric governance
     # all variables
-    name_prop_Governance = np.concatenate((name_IntCom, name_mv_modificada, name_mv_AirQ, name_mv_NoiseAte_d))
+    name_prop_Governance = np.concatenate((name_sum_pyf, name_IntCom, name_Health))
     data_prop_Governance = all_normalized_variables.loc[:, name_prop_Governance]
     num_var_governance = len(name_prop_Governance)
-    weight_var_proper1_princ5 = weight_proper_princ5 / num_var_governance
+    weight_var_proper1_princ5 = np.array([4, 13, 5]) / 180
     
-    for i in range(len(name_prop_Governance)):
+    for i in range(num_var_governance):
         y = np.array(data_prop_Governance.loc[:, name_prop_Governance[i]])
         x = np.array(time.loc[:, name_year])
         x = x.reshape(len(x),)
@@ -279,30 +274,31 @@ def slope_time_series(model_time_series, name_year,
         b1 = xx_mean_yy_mean / xx_mean2
         if b1 == 0:
             indicators[0][j] = y[0]
-            weigths[j] = weight_var_proper1_princ5
+            weigths[j] = weight_var_proper1_princ5[i]
         else:
             indicators[0][j] = b1
-            weigths[j] = weight_var_proper1_princ5
-        name_variables_indicator[j] = "Pendiente de la variable - " + name_prop_Governance[i]
+            weigths[j] = weight_var_proper1_princ5[i]
+        name_variables_indicator[j] = "Tendencia de la variable - " + name_prop_Governance[i]
         name_variables[j] = name_prop_Governance[i]
         j = j + 1
     len_5 = len(name_prop_Governance)
     
     data_variables = all_normalized_variables.loc[:, name_variables]
     data_variables_line = np.array(data_variables)
-    
     name_indicator = np.array(['Indice de resiliencia'])
     resilience_indicator = [None for x in range(len(time))]
     for i in range(len(time)):
         line = np.array(data_variables_line[i][:])
+        line[np.isnan(line)] = 0
         pond = weigths * line
-        pond_princ_1 = np.sum(pond[0:len_1] * weigths[0:len_1])
-        pond_princ_2 = np.sum(pond[len_1:len_1 + len_2] * weigths[len_1:len_1 + len_2])
-        pond_princ_3 = np.sum(pond[len_1 + len_2:len_1 + len_2 + len_3] * weigths[len_1 + len_2:len_1 + len_2 + len_3])
-        pond_princ_4 = np.sum(pond[len_1 + len_2 + len_3:len_1 + len_2 + len_3 + len_4] * weigths[len_1 + len_2 + len_3:len_1 + len_2 + len_3 + len_4])
-        pond_princ_5 = np.sum(pond[len_1 + len_2 + len_3 + len_4:len_1 + len_2 + len_3 + len_4 + len_5] * weigths[len_1 + len_2 + len_3 + len_4:len_1 + len_2 + len_3 + len_4 + len_5])
-        resilience_indicator[i] = pond_princ_1 + pond_princ_2 + pond_princ_3 + pond_princ_4 + pond_princ_5
+        resilience_indicator[i] = sum(pond)
     
+    # plt.plot(time, resilience_indicator, label = name_indicator)
+    # plt.scatter(time, resilience_indicator)
+    # plt.legend(loc='best')
+    # plt.xlabel('tiempo')
+    # plt.grid()
+    # plt.show()
     resi_indicator_DF = pd.DataFrame(resilience_indicator, columns=name_indicator).apply(pd.to_numeric)
     indicators_DF = pd.DataFrame(indicators, columns=name_variables_indicator).apply(pd.to_numeric)
     
